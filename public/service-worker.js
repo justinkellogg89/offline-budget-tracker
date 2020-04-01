9e6d8735ae3106178fcbef5e2956f2d15227be82
Justin <jlkellogg18@outlook.com>
2020-04-01 09:55:18 -0700

    added forward slashes to files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "manifest.webmanifest",
-  "index.js",
-  "styles.css",
+  "/manifest.webmanifest",
+  "/index.js",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
